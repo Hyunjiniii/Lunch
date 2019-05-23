9af9debfe7c6d98fe9df62aea06dbf44031de5d7
hyunjin <dksguswls121@naver.com>
2019-05-23 21:26:51 +0900

    환경설정 리스트뷰

--- app/src/main/java/com/example/hyunjin/lunch/MainPage/Setting/Setting.java
+++ app/src/main/java/com/example/hyunjin/lunch/MainPage/Setting/Setting.java
@@ -6,13 +6,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.hyunjin.lunch.R;
 
 public class Setting extends Fragment {
+    String[] ListMenu = {"문의하기"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_setting, container, false);
+        View view = inflater.inflate(R.layout.activity_setting, container, false);
+        ListView listView = view.findViewById(R.id.setting_listview);
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, ListMenu);
+        listView.setAdapter(adapter);
+        return view;
     }
 }
